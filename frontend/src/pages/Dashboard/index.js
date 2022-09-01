1862f84e6121161c47d1ab79bb1bd109a137472a
Manfrinne <m4nfrinne@gmail.com>
2022-09-01 12:51:45 -0300

    filter tickets by date in dashboard

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,134 +1,206 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react";
 
-import Paper from "@material-ui/core/Paper"
-import Container from "@material-ui/core/Container"
-import Grid from "@material-ui/core/Grid"
-import { makeStyles } from "@material-ui/core/styles"
+import DateFnsUtils from "@date-io/date-fns";
+import { isWithinInterval, parseISO } from 'date-fns';
+
+import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
 
-import useTickets from "../../hooks/useTickets"
+import useTickets from "../../hooks/useTickets";
 
 import { AuthContext } from "../../context/Auth/AuthContext";
 
 import { i18n } from "../../translate/i18n";
 
-import Chart from "./Chart"
+import Chart from "./Chart";
 import UsersList from "./UsersList";
-import DateTime from "./DateTime"
 
-const useStyles = makeStyles(theme => ({
-	container: {
-		paddingTop: theme.spacing(4),
-		paddingBottom: theme.spacing(4),
-	},
-	fixedHeightPaper: {
-		padding: theme.spacing(2),
-		display: "flex",
-		overflow: "auto",
-		flexDirection: "column",
-		height: 240,
-	},
-	customFixedHeightPaper: {
-		padding: theme.spacing(2),
-		display: "flex",
-		overflow: "auto",
-		flexDirection: "column",
-		height: 120,
-	},
-	customFixedHeightPaperLg: {
-		padding: theme.spacing(2),
-		display: "flex",
-		overflow: "auto",
-		flexDirection: "column",
-		height: "100%",
-	},
+const useStyles = makeStyles((theme) => ({
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  fixedHeightPaper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column",
+    height: 240,
+  },
+  customFixedHeightPaper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column",
+    height: 120,
+  },
+  customFixedHeightPaperLg: {
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column",
+    height: "100%",
+  },
   customFixedHeightPaperDateTime: {
-		padding: theme.spacing(2),
-		display: "flex",
-		overflow: "auto",
-		flexDirection: "column",
-		height: "100%",
-	},
-}))
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column",
+    height: "100%",
+  },
+}));
 
 const Dashboard = () => {
-	const classes = useStyles()
+  const classes = useStyles();
 
-	const { user } = useContext(AuthContext);
-	var userQueueIds = [];
+  const [selectedStartDate, setSelectedStartDate] = useState(
+    new Date("2022-01-01T00:00:00")
+  );
 
-	if (user.queues && user.queues.length > 0) {
-		userQueueIds = user.queues.map(q => q.id);
-	}
+  const [selectedEndDate, setSelectedEndDate] = useState(
+    new Date("2022-12-31T23:00:00")
+  );
 
-	const GetTickets = (status, showAll, withUnreadMessages) => {
+  const { user } = useContext(AuthContext);
+  var userQueueIds = [];
 
-		const { count } = useTickets({
-			status: status,
-			showAll: showAll,
-			withUnreadMessages: withUnreadMessages,
-			queueIds: JSON.stringify(userQueueIds)
-		});
-		return count;
-	}
+  if (user.queues && user.queues.length > 0) {
+    userQueueIds = user.queues.map((q) => q.id);
+  }
 
-	return (
-		<div>
-			<Container maxWidth="lg" className={classes.container}>
-				<Grid container spacing={3}>
+  const handleDateStartChange = (date) => {
+    setSelectedStartDate(date);
+  };
+
+  const handleDateEndChange = (date) => {
+    setSelectedEndDate(date);
+  };
+
+  const GetTickets = (status, showAll, withUnreadMessages) => {
+    const { tickets } = useTickets({
+      status: status,
+      showAll: showAll,
+      withUnreadMessages: withUnreadMessages,
+      queueIds: JSON.stringify(userQueueIds),
+    });
+
+    // eslint-disable-next-line
+    const ticketFilteredByDate = tickets.filter((ticket) => {
+      const ticketInsideDate = isWithinInterval(parseISO(ticket.createdAt), {
+        start: selectedStartDate,
+        end: selectedEndDate,
+      });
+
+      if (ticketInsideDate) return ticket
+    })
+
+    return ticketFilteredByDate.length;
+  };
+
+  return (
+    <div>
+      <Container maxWidth="lg" className={classes.container}>
+        <Grid container spacing={3}>
           <Grid item xs={12}>
             <Paper className={classes.customFixedHeightPaperDateTime}>
-              <DateTime />
-						</Paper>
-					</Grid>
-					<Grid item xs={4}>
-						<Paper className={classes.customFixedHeightPaper} style={{ overflow: "hidden" }}>
-							<Typography component="h3" variant="h6" color="primary" paragraph>
-								{i18n.t("dashboard.messages.inAttendance.title")}
-							</Typography>
-							<Grid item>
-								<Typography component="h1" variant="h4">
-									{GetTickets("open", "true", "false")}
-								</Typography>
-							</Grid>
-						</Paper>
-					</Grid>
-					<Grid item xs={4}>
-						<Paper className={classes.customFixedHeightPaper} style={{ overflow: "hidden" }}>
-							<Typography component="h3" variant="h6" color="primary" paragraph>
-								{i18n.t("dashboard.messages.waiting.title")}
-							</Typography>
-							<Grid item>
-								<Typography component="h1" variant="h4">
-									{GetTickets("pending", "true", "false")}
-								</Typography>
-							</Grid>
-						</Paper>
-					</Grid>
-					<Grid item xs={4}>
-						<Paper className={classes.customFixedHeightPaper} style={{ overflow: "hidden" }}>
-							<Typography component="h3" variant="h6" color="primary" paragraph>
-								{i18n.t("dashboard.messages.closed.title")}
-							</Typography>
-							<Grid item>
-								<Typography component="h1" variant="h4">
-									{GetTickets("closed", "true", "false")}
-								</Typography>
-							</Grid>
-						</Paper>
-					</Grid>
-					<Grid item xs={12}>
-						<Paper className={classes.fixedHeightPaper}>
-							<Chart />
-						</Paper>
-					</Grid>
-				</Grid>
+
+              {/* Determinar data inicial e final dos tickets exibidos */}
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <Grid container justifyContent="space-evenly">
+                  <KeyboardDatePicker
+                    disableToolbar
+                    variant="inline"
+                    format="dd/MM/yyyy"
+                    margin="normal"
+                    id="date-picker-inline"
+                    label="Data ticket - inicial"
+                    value={selectedStartDate}
+                    onChange={handleDateStartChange}
+                    KeyboardButtonProps={{
+                      "aria-label": "change date",
+                    }}
+                  />
+                  <KeyboardDatePicker
+                    disableToolbar
+                    variant="inline"
+                    format="dd/MM/yyyy"
+                    margin="normal"
+                    id="date-picker-inline"
+                    label="Data ticket - final"
+                    value={selectedEndDate}
+                    onChange={handleDateEndChange}
+                    KeyboardButtonProps={{
+                      "aria-label": "change date",
+                    }}
+                  />
+                </Grid>
+              </MuiPickersUtilsProvider>
+
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper
+              className={classes.customFixedHeightPaper}
+              style={{ overflow: "hidden" }}
+            >
+              <Typography component="h3" variant="h6" color="primary" paragraph>
+                {i18n.t("dashboard.messages.inAttendance.title")}
+              </Typography>
+              <Grid item>
+                <Typography component="h1" variant="h4">
+                  {GetTickets("open", "true", "false")}
+                </Typography>
+              </Grid>
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper
+              className={classes.customFixedHeightPaper}
+              style={{ overflow: "hidden" }}
+            >
+              <Typography component="h3" variant="h6" color="primary" paragraph>
+                {i18n.t("dashboard.messages.waiting.title")}
+              </Typography>
+              <Grid item>
+                <Typography component="h1" variant="h4">
+                  {GetTickets("pending", "true", "false")}
+                </Typography>
+              </Grid>
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper
+              className={classes.customFixedHeightPaper}
+              style={{ overflow: "hidden" }}
+            >
+              <Typography component="h3" variant="h6" color="primary" paragraph>
+                {i18n.t("dashboard.messages.closed.title")}
+              </Typography>
+              <Grid item>
+                <Typography component="h1" variant="h4">
+                  {GetTickets("closed", "true", "false")}
+                </Typography>
+              </Grid>
+            </Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.fixedHeightPaper}>
+              <Chart />
+            </Paper>
+          </Grid>
+        </Grid>
 
         <UsersList />
+      </Container>
+    </div>
+  );
+};
 
-			</Container>
-		</div>
-	)
-}
-
-export default Dashboard
+export default Dashboard;
